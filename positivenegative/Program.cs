3364300899bd687d8611c4acd79a8bb51614535a
super <super@LAPTOP-3LCJEP17>
2021-11-05 20:53:50 +0300

    Add project files.

---
+++ positivenegative/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace positivenegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int sayi;
+            Console.WriteLine("Bir sayı giriniz.");
+            sayi =int.Parse(Console.ReadLine());
+            
+            
+             
+            if (sayi < 0)
+            {
+                Console.WriteLine("Girdiğiniz sayı negatif.");
+
+            }
+            else if(sayi>0)
+            {
+                Console.WriteLine("Girdiğiniz sayı pozitif.");
+
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz sayı sıfır.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
